bf0c2155cb482118ed22177b98ac4abbf51788dc
Dar Brett <dar.brett.development@gmail.com>
2016-10-02 13:03:31 +0800

    Server can be built and mods downloaded

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,14 @@
-scp mcmod-add mcmod-add-version mcmod-update-version dar@192.168.0.242:/var/opt/mcmod/
+scp -r \
+mcmod-add \
+mcmod-add-version \
+mcmod-update-version \
+mcmod-download \
+mcmod-func.shinc \
+mcmod-get-version-url \
+mcmod-server-build \
+mcmod-update-version \
+mcmod-resolve-mods \
+mods \
+minecraft-forge \
+include \
+dar@192.168.0.242:/opt/mcmod/
---
+++ include/functions.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+field_value() {
+        KEY=$1
+        FILE=$2
+
+        REGEX="^$KEY="
+        LINE=$(grep $REGEX $FILE)
+
+        if [[ -z $LINE ]]; then
+                echo "$KEY not found" >&2
+                exit 1
+        fi
+        RESULT=$(cut -d "=" -f 2 <<< $LINE)
+
+        echo $RESULT
+}
+
+cached_download() {
+FILE=$1
+URL=$2
+if [ -f $FILE ]; then
+        echo "File already exists - nothing to do"
+        return 0
+fi
+
+if [ ! -d "$(dirname $0)/cache/" ]; then
+        mkdir "$(dirname $0)/cache/"
+fi
+
+CACHE="$(dirname $0)/cache/$FILE"
+
+if [ ! -f $CACHE ]; then
+# Logic to download past bot protection on Curse
+if [[ $URL =~ ^https:\/\/mods\.curse\.com\/mc-mods\/minecraft\/ ]]; then
+URL=$(wget -O - -o /dev/null  $URL |  sed ':a:N:$!ba;s/\"/\n/g' \
+| grep http://addons.curse.cursecdn.com/)
+echo "Sleeping for 2 seconds to comply with Curse Terms of Service"
+sleep 2
+fi
+
+echo "Downloading $URL"
+
+wget $URL -O $CACHE -nc
+fi
+echo "Moving to Working directory"
+cp $CACHE $FILE
+}
