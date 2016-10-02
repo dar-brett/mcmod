0d36d386a1e63fa4a4c1b2f76b01bc005da986da
Dar Brett <dar.brett.development@gmail.com>
2016-10-02 13:24:50 +0800

    Fixed a bug adding junk files when failing to add mods that exist, and added some Forestry versions

--- populate-known-mods.sh
+++ populate-known-mods.sh
@@ -11,3 +11,9 @@
 ./mcmod-add-version thaumcraft 1.7.10 4.2.3.2 https://mods.curse.com/mc-mods/minecraft/223628-thaumcraft/2224347
 ./mcmod-add-version thaumcraft 1.7.10 4.2.3.1 https://mods.curse.com/mc-mods/minecraft/223628-thaumcraft/2224297
 ./mcmod-add-version thaumcraft 1.7.10 4.2.3.0 https://mods.curse.com/mc-mods/minecraft/223628-thaumcraft/2223710
+
+./mcmod-add forestry https://mods.curse.com/mc-mods/minecraft/forestry
+./mcmod-add-version forestry 1.7.10 4.2.15 https://mods.curse.com/mc-mods/minecraft/forestry/2320118
+./mcmod-add-version forestry 1.7.10 4.2.14 https://mods.curse.com/mc-mods/minecraft/forestry/2309923
+./mcmod-add-version forestry 1.7.10 4.2.13 https://mods.curse.com/mc-mods/minecraft/forestry/2306433
+./mcmod-add-version forestry 1.7.10 4.2.12 https://mods.curse.com/mc-mods/minecraft/forestry/2296588
